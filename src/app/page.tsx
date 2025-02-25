9c737e0c235e8bd9892101dd39af17613f516da7
chan9yu <dev.cgyeo@gmail.com>
2025-02-25 13:04:23 +0900

    feat: static export 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 export default function RootPage() {
 	return (
-		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">
-			<span className="font-thin">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-extralight">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-light">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-normal">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-medium">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-semibold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-bold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-extrabold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-			<span className="font-black">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
-		</div>
+		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">Root Page</div>
 	);
 }
---
+++ src/app/test/page.tsx
@@ -0,0 +1,5 @@
+export default function TestPage() {
+	return (
+		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">Test Page</div>
+	);
+}
