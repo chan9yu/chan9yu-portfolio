dbfdb6625b2a6e23f88ceccb87910dc27311caad
chan9yu <dev.cgyeo@gmail.com>
2025-02-27 13:26:48 +0900

    feat: 링크 테스트 코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
 export default function RootPage() {
 	return (
-		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">Root Page</div>
+		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">
+			Root Page
+			<Link href="/test" className="text-blue-500 underline">
+				Move To Test Page
+			</Link>
+		</div>
 	);
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
 export default function TestPage() {
 	return (
-		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">Test Page</div>
+		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">
+			Test Page
+			<Link href="/" className="text-blue-500 underline">
+				Move To Root Page
+			</Link>
+		</div>
 	);
 }
